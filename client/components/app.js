207ecf2db37e296ee948974be7b1f42c3be31723
LPT <lptalty@gmail.com>
2021-09-13 14:48:06 -0400

    first commit

---
+++ client/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div> This is React hooks</div>
+      <div> testing stil? </div>
+    </div>
+  );
+};
+
+export default App;
