a8efddb2b5902de818bfb67b2bcfab44e287afdd
n-tracy1 <n.tracy@outlook.com>
2021-09-14 12:35:48 -0400

    exoplanet minor skeleton update

--- server/controllers/exoplanet.js
+++ server/controllers/exoplanet.js
@@ -1,4 +1,13 @@
 
 const exoplanet = {};
 
+//make API call and pass back JSON object to the frontend
+exoplanet.getData = function(req, res, next) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
 module.exports = exoplanet;
